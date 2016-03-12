ce5ac692a18d00b43994b24d85408533835620dc
Matthew Neil <matthew.neil84@gmail.com>
2016-03-12 14:14:05 +0000

    First commit with folder and test gulp config

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path =  require('path');
+
+var basePaths = {
+  src: 'src/';
+  dist: 'dist/'
+};
+
+var paths = {
+  styles: {
+    src: basePaths + 'css/';
+    dist: basePaths + 'css/'
+  }
+};
+// notes passing variables does not work!
+gulp.task('default', function (options) {
+  return gulp.src(styles.src + '*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, './src/', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist/css'));
+});
